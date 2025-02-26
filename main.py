4b8a8326e921103acbac904160977ae14c564e08
Iliya Badri <133355601+IliyaBadri@users.noreply.github.com>
2025-02-26 12:13:59 +0330

    Some structure changes ( again )
    [Untested code!]

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, send_from_directory, request, jsonify
 import json
 import logging
 import os
+import random
 import time
 import threading
 import torch
@@ -10,37 +11,17 @@ from typing import Callable, List, Optional
 def print_exception(exception: Exception):
     raise exception
 
-
 def print_tokens(tokens: list[str]):
     for token in tokens:
         print(token, end='', flush=True)
 
-class TokenEventSystem():
-    subscriber_type = Callable[[List[str]], None]
-    token_queue: List[str] = []
-    subscribers: List[subscriber_type] = []
+def generate_unique_id() -> str:
+    timestamp = int(time.time())
+    random_digits = random.randint(1000, 9999) 
+    unique_id = f"{timestamp}{random_digits}"
+    return unique_id
 
-    @staticmethod
-    def subscribe(subscriber: subscriber_type):
-        TokenEventSystem.subscribers.append(subscriber) 
 
-    @staticmethod
-    def add_tokens(tokens: List[str]):
-        TokenEventSystem.token_queue.extend(tokens)
-
-    @staticmethod
-    def create_worker():
-        def worker():
-            while True:
-                if(len(TokenEventSystem.token_queue) > 0):
-                    for subscriber in TokenEventSystem.subscribers:
-                        subscriber(TokenEventSystem.token_queue)
-                    TokenEventSystem.token_queue.clear()
-                else:
-                    time.sleep(0.1)
-
-        thread = threading.Thread(target=worker, daemon=True)
-        thread.start()
 
 class Model():
     def __init__(self):
@@ -48,19 +29,23 @@ class Model():
         self.model: GPT2LMHeadModel = GPT2LMHeadModel.from_pretrained(self.model_name)
         self.tokenizer: GPT2Tokenizer = GPT2Tokenizer.from_pretrained(self.model_name)
 
-class GenerationSession():
-    def __init__(self, model: Model, prompt: str):
-        self.model = model
-        self.prompt = prompt
-        self.prompt_tensor: torch.Tensor = self.model.tokenizer.encode(prompt, return_tensors="pt")
-        self.output_tensor = self.prompt_tensor
+    @staticmethod
+    def get_subtensor_from_end(tensor: torch.Tensor, token_count: int) -> torch.Tensor:
+        selected_tokens = []
+        for i in range(token_count):
+            selected_tokens.append(tensor[0, -(token_count - i)]) 
+        subtensor = torch.tensor([selected_tokens]) 
+        return subtensor
+
+    def initialize_output_tensor(self, prompt: str) -> torch.Tensor:
+        return self.tokenizer.encode(prompt, return_tensors="pt")
     
-    def get_attention_mask(self) -> torch.Tensor:
+    def get_attention_mask(self, output_tensor: torch.Tensor) -> torch.Tensor:
         attention_mask_array = []
-        for sequence in self.output_tensor:
+        for sequence in output_tensor:
             mask = []
             for token in sequence:
-                if token == self.model.tokenizer.eos_token_id:
+                if token == self.tokenizer.eos_token_id:
                     mask.append(0)
                 else:
                     mask.append(1)
@@ -69,94 +54,81 @@ class GenerationSession():
 
         return torch.tensor(attention_mask_array)
     
-    def append_delta(self, generated_tensor: torch.Tensor, token_count: int) -> torch.Tensor:
-        generated_tokens = []
-        for i in range(token_count):
-            generated_tokens.append(generated_tensor[0, -(token_count - i)]) 
-        delta_tensor = torch.tensor([generated_tokens]) 
-        self.output_tensor = torch.cat((self.output_tensor, delta_tensor), dim=1)
-        return delta_tensor
+    def decode_tokens_tensor(self, tokens_tensor: torch.Tensor) -> List[str]:
+        tokens: list[str] = []
+
+        for token_id in tokens_tensor[0]:
+            tokens.append(self.tokenizer.decode(token_id, skip_special_tokens=True))
+        
+        return tokens
     
-    def generate_tokens(self, token_count: int) -> Optional[List[str]]:
-        attention_mask = self.get_attention_mask()
+    def generate_tokens(self, output_tensor: torch.Tensor, token_count: int) -> Optional[list[str]]:
+        if len(output_tensor[0]) > 0:
+            if output_tensor [0, -1] == self.tokenizer.eos_token_id:
+                return None
+        
+        attention_mask = self.get_attention_mask(output_tensor)
 
         generated_tensor = self.model.model.generate(
-            self.output_tensor,
-            max_length = self.output_tensor.shape[1] + token_count,
+            output_tensor,
+            max_length = output_tensor.shape[1] + token_count,
             num_return_sequences = 1, 
             no_repeat_ngram_size = 2,
             top_p = 0.92,
             temperature=0.82,
             do_sample=True,
-            pad_token_id=self.model.tokenizer.eos_token_id,
+            pad_token_id=self.tokenizer.eos_token_id,
             attention_mask=attention_mask
         )
 
-        delta_tensor = self.append_delta(generated_tensor, token_count)
+        new_tokens_tensor = Model.get_subtensor_from_end(generated_tensor, token_count)
 
-        tokens: list[str] = []
+        output_tensor[:] = torch.cat((output_tensor, new_tokens_tensor), dim=1)
 
-        for i in range(len(delta_tensor[0])):
-            tokens.append(self.model.tokenizer.decode(delta_tensor[0, i], skip_special_tokens=True))
+        tokens = self.decode_tokens_tensor(new_tokens_tensor)
 
         return tokens
+
+    
+
+class TokenList():
+    def __init__(self, tokens: List[str], prompt_id: str):
+        self.id = prompt_id,
+        self.tokens = tokens
+
+class Prompt():
+    def __init__(self, model: Model, prompt: str):
+        self.id = generate_unique_id()
+        self.model = model
+        self.prompt = prompt
+        self.output_tensor: torch.Tensor = self.model.initialize_output_tensor(prompt)
     
     def generate_recursively(self, rounds: int, tokens_per_round: int):
         try:
             for _ in range(rounds):
-                tokens = self.generate_tokens(tokens_per_round)
+                tokens = self.model.generate_tokens(self.output_tensor, tokens_per_round)
                 if(tokens == None):
                     break
-            TokenEventSystem.add_tokens(tokens)
+                token_list = TokenList(tokens, self.id)
+                Pools.add_token_list(token_list)
         except Exception as exception:
             print_exception(exception)
+
+class Pools():
+    prompts: List[Prompt] = []
+    token_lists: List[TokenList] = []
+
+    @staticmethod
+    def add_prompt(prompt: Prompt):
+        Pools.prompts.append(prompt)
+
+    def add_token_list(token_list: TokenList):
+        Pools.token_lists.append(token_list)
 
 def web_server_worker(model: Model, port=8000):
     static_path = os.path.abspath("./static")
     app = Flask(__name__, static_folder=static_path)
     logging.getLogger('werkzeug').disabled = True
-
-    @app.route("/<path:filename>")
-    def serve_static(filename):
-        return send_from_directory(static_path, filename)
-    
-    @app.route("/generate", methods=["POST"])
-    def generate():
-        if request.method == 'POST':
-            prompt = request.json["prompt"]
-            if not prompt or prompt == None or prompt == "":
-                return jsonify({"successful": False})
-            
-            try:
-                session = GenerationSession(model, prompt)
-
-                def generate_async():
-                    session.generate_recursively(40, 5)  
-            
-                generation_thread = threading.Thread(target=generate_async, daemon=True)
-                generation_thread.start()
-
-                return jsonify({"successful": True})
-            except Exception as exception:
-                print_exception(exception)
-                return jsonify({"successful": False})
-
-    @app.route("/updates")
-    def sse():
-        def event_stream():
-            tokens: List[str] = []
-            TokenEventSystem.subscribe(tokens.extend)
-            while True:
-                if len(tokens) > 0:
-                    tokens_to_send = []
-                    tokens_to_send.extend(tokens)
-                    tokens.clear()
-                    yield f"data: {json.dumps({'tokens': tokens_to_send})}\n\n"
-                else:
-                    time.sleep(0.1)
-
-        return Response(event_stream(), content_type="text/event-stream")
-    
     print(f"[+] HTTP server is up ( http://localhost:{port}/index.html )")
     app.run("", port, threaded=True)
 
@@ -170,9 +142,9 @@ if __name__ == "__main__":
     from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
     model = Model()
-    TokenEventSystem.create_worker()
-    TokenEventSystem.subscribe(print_tokens)
 
     web_server_worker(model, 8000)
+
+
 
     
