1a524169fa5c4c8f9d97b60e055f3a7286177023
Iliya Badri <133355601+IliyaBadri@users.noreply.github.com>
2025-02-27 21:46:08 +0330

    Multiple changes
    - Added config file ( for now not effective on front end )
    
    - Added multiple client support

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import asyncio
 import json
-import logging
-from multiprocessing import Process
+import multiprocessing
 import os
 import random
+import signal
+import sys
 import time
 import torch
 import http.server
 import socketserver
 import websockets
-from typing import Callable, List, Optional
-
-def print_exception(exception: Exception):
-    raise exception
-
-def print_tokens(tokens: list[str]):
-    for token in tokens:
-        print(token, end='', flush=True)
+from typing import List, Optional
 
 def generate_unique_id() -> str:
     timestamp = int(time.time())
@@ -35,6 +29,10 @@ class Model():
     @staticmethod
     def get_subtensor_from_end(tensor: torch.Tensor, token_count: int) -> torch.Tensor:
         selected_tokens = []
+
+        if token_count > len(tensor[0]):
+            return tensor
+        
         for i in range(token_count):
             selected_tokens.append(tensor[0, -(token_count - i)]) 
         subtensor = torch.tensor([selected_tokens]) 
@@ -52,24 +50,19 @@ class Model():
                     mask.append(0)
                 else:
                     mask.append(1)
-
             attention_mask_array.append(mask)
-
         return torch.tensor(attention_mask_array)
     
     def decode_tokens_tensor(self, tokens_tensor: torch.Tensor) -> List[str]:
         tokens: list[str] = []
-
         for token_id in tokens_tensor[0]:
             tokens.append(self.tokenizer.decode(token_id, skip_special_tokens=True))
-        
         return tokens
     
     def generate_tokens(self, output_tensor: torch.Tensor, token_count: int) -> tuple[Optional[list[str]], Optional[torch.Tensor]]:
         if len(output_tensor[0]) > 0:
             if output_tensor [0, -1] == self.tokenizer.eos_token_id:
                 return None, None
-        
         attention_mask = self.get_attention_mask(output_tensor)
 
         generated_tensor = self.model.generate(
@@ -85,9 +78,7 @@ class Model():
         )
 
         new_tokens_tensor = Model.get_subtensor_from_end(generated_tensor, token_count)
-
         tokens = self.decode_tokens_tensor(new_tokens_tensor)
-
         return tokens, new_tokens_tensor
 
 class TokenList():
@@ -104,20 +95,22 @@ class Prompt():
     
     def generate_recursively(self, rounds: int, tokens_per_round: int):
         try:
+            token_list = TokenList([f"\n#{self.id}: ", self.prompt], self.id)
+            Pools.add_token_list(token_list)
             for _ in range(rounds):
                 tokens, new_tokens_tensor = self.model.generate_tokens(self.output_tensor, tokens_per_round)
                 if tokens == None or new_tokens_tensor == None:
                     break
                 self.output_tensor = torch.cat((self.output_tensor, new_tokens_tensor), dim=1)
                 token_list = TokenList(tokens, self.id)
-                print_tokens(token_list.tokens) #remove for production
                 Pools.add_token_list(token_list)
         except Exception as exception:
-            print_exception(exception)
+            raise exception
 
 class Pools():
     prompts: List[Prompt] = []
     token_lists: List[TokenList] = []
+    websocket_list: List[websockets.ServerConnection] = []
 
     @staticmethod
     def add_prompt(prompt: Prompt):
@@ -146,83 +139,106 @@ class StaticServerHandler(http.server.SimpleHTTPRequestHandler):
             self.path = "/static/index.html"
         return super().do_GET()
     
-def start_static_server(port=8000):
+def start_static_server(port: int, multiprocessing_shutdown_event):
     with socketserver.TCPServer(("", port), StaticServerHandler) as httpd:
         print(f"[+] Web GUI is up at port http://localhost:{port}/")
-        httpd.serve_forever()
+        while not multiprocessing_shutdown_event.is_set():
+            httpd.handle_request()
 
-async def start_token_generator():
-    while True:
+async def start_token_generator(asyncio_shutdown_signal: asyncio.Event):
+    while not asyncio_shutdown_signal.is_set():
         prompt = Pools.pull_prompt()
         if prompt == None:
             await asyncio.sleep(0.1)
             continue
-
         prompt.generate_recursively(10, 5)
-
-async def handle_websocket_messages(websocket):
-    while True:
-        message = await websocket.recv()
-        try:
-            data = json.loads(message)
-            prompt_string = data["prompt"]
-
-            if(type(prompt_string) != str):
-                print("[-] Invalid JSON received")
-                continue
-            
-            if(Model.global_model == None):
-                continue
-
-            prompt = Prompt(Model.global_model, prompt_string)
-
-            print(f"[+] A new prompt has been added to the pool. ( prompt id: {prompt.id} )")
-
-            Pools.add_prompt(prompt)
-
-        except json.JSONDecodeError:
-            print("[-] Invalid JSON received")
-
         await asyncio.sleep(0.1)
 
-
-async def websocket_handler(websocket):
+async def websocket_connection_handler(websocket, asyncio_shutdown_event: asyncio.Event):
     print(f"[+] New WebSocket connection: {websocket.remote_address}")
+    Pools.websocket_list.append(websocket)
     try:
-        token_sender_task = asyncio.create_task(handle_websocket_messages(websocket))
-        while True:
-            token_list = Pools.pull_token_list()
-            if token_list == None:
-                await asyncio.sleep(0.1)
-                continue
+        while not asyncio_shutdown_event.is_set():
+            message = await websocket.recv()
+            try:
+                data = json.loads(message)
+                prompt_string = data["prompt"]
+
+                if(type(prompt_string) != str):
+                    print("[-] Invalid JSON received")
+                    continue
+                
+                if(Model.global_model == None):
+                    continue
+
+                prompt = Prompt(Model.global_model, prompt_string)
+
+                print(f"[+] A new prompt has been added to the pool. ( prompt id: {prompt.id} )")
+
+                Pools.add_prompt(prompt)
+            except json.JSONDecodeError:
+                print("[-] Invalid JSON received")
+        await asyncio.sleep(0.1)
+    except websockets.exceptions.ConnectionClosed as e:
+        Pools.websocket_list.remove(websocket)
+        print(f"[-] Websocket connection closed: {e}")
+    finally:
+        Pools.websocket_list.remove(websocket)
+
+async def start_websocket_token_sender(asyncio_shutdown_event: asyncio.Event):
+    while not asyncio_shutdown_event.is_set():
+        token_list = Pools.pull_token_list()
+        if token_list == None:
+            await asyncio.sleep(0.1)
+            continue
+        for websocket in Pools.websocket_list:
             response = {
                 "prompt_id": token_list.id,
                 "tokens": token_list.tokens
             }
             await websocket.send(json.dumps(response))
-            await asyncio.sleep(0.1)
-    except websockets.exceptions.ConnectionClosed as e:
-        print(f"[-] Websocket connection closed: {e}")
+        await asyncio.sleep(0.1)
 
-    finally:
-        token_sender_task.cancel()
-
-async def start_websocket_server(port=8001):
-    server = await websockets.serve(websocket_handler, "localhost", port)
+async def start_websocket_server(port: int, asyncio_shutdown_event: asyncio.Event):
+    server = await websockets.serve(lambda websocket: websocket_connection_handler(websocket, asyncio_shutdown_event), "localhost", port)
     print(f"[+] WebSocket server started at ws://localhost:{port}")
+    await start_websocket_token_sender(asyncio_shutdown_event)
+    await asyncio_shutdown_event.wait()
+    server.close()
     await server.wait_closed()
 
-async def main_asyncio_coroutine():
-    token_generator_task = asyncio.create_task(start_token_generator())
-    websocket_server_task = asyncio.create_task(start_websocket_server())
+def handle_shutdown(the_signal, frame, asyncio_shutdown_event: asyncio.Event, multiprocessing_shutdown_event):
+    print(f"[-] Recieved exit signal {the_signal}")
+    print("[-] Terminating")
+    asyncio_shutdown_event.set()
+    multiprocessing_shutdown_event.set()
 
-    await asyncio.gather(token_generator_task, websocket_server_task)
+async def main_asyncio_coroutine(websocket_port: int, asyncio_shutdown_signal: asyncio.Event):
+    websocket_server_task = asyncio.create_task(start_websocket_server(WEBSOCKET_PORT, asyncio_shutdown_signal))
+    token_generator_task = asyncio.create_task(start_token_generator(multiprocessing_shutdown_event))
+    await asyncio.gather(websocket_server_task, token_generator_task)
 
 if __name__ == "__main__":
-    models_path = os.path.abspath("./models")
-    os.makedirs(models_path, exist_ok=True)
+    try:
+        with open("config.json", "r") as config_file:
+            config = json.load(config_file)
+            STATIC_PORT = config["static_port"]
+            WEBSOCKET_PORT = config["websocket_port"]
+    except:
+        print("[-] Error reading config.json")
+        print("[-] Terminating")
+        sys.exit(1)
 
-    os.environ["HF_HOME"] = models_path
+    if type(STATIC_PORT) != int or type(WEBSOCKET_PORT) != int:
+        print("[-] Invalid port configuration in config.json")
+        print("[-] Terminating")
+        sys.exit(1)
+
+    MODELS_PATH = os.path.abspath("./models")
+    os.makedirs(MODELS_PATH, exist_ok=True)
+
+    print(f"[+] Models path is set to: {MODELS_PATH}")
+    os.environ["HF_HOME"] = MODELS_PATH
     os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
 
     from transformers import GPT2LMHeadModel, GPT2Tokenizer
@@ -231,13 +247,17 @@ if __name__ == "__main__":
     model = Model()
     print(f"[+] The {model.model_name} model has been loaded.")
 
-    static_server_process = Process(target=start_static_server)
+    asyncio_shutdown_event = asyncio.Event()
+    multiprocessing_shutdown_event = multiprocessing.Event()
+
+    signal.signal(signal.SIGINT, lambda the_signal, frame: handle_shutdown(the_signal, frame, asyncio_shutdown_event, multiprocessing_shutdown_event))
+    signal.signal(signal.SIGTERM, lambda the_signal, frame: handle_shutdown(the_signal, frame, asyncio_shutdown_event, multiprocessing_shutdown_event))  
+
+    static_server_process = multiprocessing.Process(target=start_static_server, args=(STATIC_PORT, multiprocessing_shutdown_event))
     static_server_process.start()
 
-    asyncio.run(main_asyncio_coroutine())
-
-    
-
-
-
-    
+    try:
+        asyncio.run(main_asyncio_coroutine(WEBSOCKET_PORT, asyncio_shutdown_event))
+    finally:
+        static_server_process.terminate()
+        static_server_process.join()
